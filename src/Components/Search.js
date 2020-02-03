c209058726340cd7a284003dd2107ac4b1680a90
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 19:50:47 +0100

    fix(): Add chevron down icon for the dropdown menu

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../Style/Search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
   return (
     <section>
       <div class='container'>
+        
         <input id='search' class='search' type='text' placeholder='Search for a country' />
         <div id='filter' class='dropdown'>
           Filter by region
-          <i class='fas fa-chevron-down'></i>
+          <FontAwesomeIcon className='fas fa-chevron-down' icon={faChevronDown} />
           <ul>
             <li>All</li>
             <li>Africa</li>
