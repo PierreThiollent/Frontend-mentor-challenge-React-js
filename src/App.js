b07d3c0aeea04c88da384abb72e00d1411478587
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 21:02:11 +0100

    feat(): Use List component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Style/App.css';
 import Header from './Components/Header';
 import Search from './Components/Search';
+import List from './Components/List';
 
 const App = () => (
   <>
     <Header />
     <Search />
+    <List />
   </>
 );
 
