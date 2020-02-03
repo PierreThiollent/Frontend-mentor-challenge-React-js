070af339e7e256d245275da4c348a16cc66cdf06
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 19:58:53 +0100

    feat(): Add logic (state) to show or hide the dropdown menu

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Style/Search.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
+  // State to show or hide dropdown menu
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   return (
     <section>
       <div class='container'>
-        
         <input id='search' class='search' type='text' placeholder='Search for a country' />
-        <div id='filter' class='dropdown'>
+        <div id='filter' class='dropdown' onClick={() => setDropdownVisible(!dropdownVisible)}>
           Filter by region
           <FontAwesomeIcon className='fas fa-chevron-down' icon={faChevronDown} />
-          <ul>
-            <li>All</li>
-            <li>Africa</li>
-            <li>America</li>
-            <li>Asia</li>
-            <li>Europe</li>
-            <li>Oceania</li>
-          </ul>
+          {dropdownVisible && (
+            <ul>
+              <li>All</li>
+              <li>Africa</li>
+              <li>America</li>
+              <li>Asia</li>
+              <li>Europe</li>
+              <li>Oceania</li>
+            </ul>
+          )}
         </div>
       </div>
     </section>
