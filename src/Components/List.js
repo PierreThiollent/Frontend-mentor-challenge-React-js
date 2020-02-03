cc9158fa4dadfcf95b7c2bc1f16d171a3327bd1c
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 22:50:24 +0100

    feat(): Add loader during the data fecth

--- src/Components/List.js
+++ src/Components/List.js
@@ -7,20 +7,32 @@ const List = () => {
   // State to store countries
   const [countries, setcountries] = useState([]);
 
+  // State to mange the loading spinner
+  const [loading, setLoading] = useState(true);
+
   // Effect to fetch countries from the API
   useEffect(() => {
     (async () => {
       await GetCountries().then(countriesList => {
         setcountries(countriesList);
+        setLoading(false);
       });
     })();
   }, [setcountries]);
 
-  return (
-    <div id='countries' className='container countries'>
+  return loading ? (
+    <div className='lds-ring'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  ) : (
+    <div className='container countries'>
       {countries.map((country, index) => (
         <Item country={country} key={index} />
       ))}
+      }
     </div>
   );
 };
