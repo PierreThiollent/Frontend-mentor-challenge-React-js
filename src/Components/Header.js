03a8f3fc53be39764db9c162f204724e69cc578a
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 20:58:25 +0100

    feat(): Add method to toggle dark mode

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,17 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon as faMoonDark } from '@fortawesome/free-solid-svg-icons';
 import { faMoon as faMoonLignt } from '@fortawesome/free-regular-svg-icons';
 
-const Header = () => (
-  <header>
-    <div class='container'>
-      <h1>Where in the world</h1>
-      <button id='toggle' class='btn-toggle-dark-mode'>
-        <FontAwesomeIcon className='far fa-moon' icon={faMoonDark} />
-        <FontAwesomeIcon className='fas fa-moon' icon={faMoonLignt} />
-        Dark mode
-      </button>
-    </div>
-  </header>
-);
+const Header = () => {
+  // Method to toggle dark mode
+  const toggleDarkMode = () => {
+    document.body.classList.toggle('dark');
+  };
+
+  return (
+    <header>
+      <div className='container'>
+        <h1>Where in the world</h1>
+        <button id='toggle' className='btn-toggle-dark-mode' onClick={toggleDarkMode}>
+          <FontAwesomeIcon className='far fa-moon' icon={faMoonDark} />
+          <FontAwesomeIcon className='fas fa-moon' icon={faMoonLignt} />
+          Dark mode
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
