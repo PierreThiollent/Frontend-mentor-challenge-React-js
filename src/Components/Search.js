e3cc86eeef2310f06c13ccca7f0b5bb0847c9162
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-03 22:53:07 +0100

    feat(): Add state and method to manage the input value

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,13 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
-  // State to show or hide dropdown menu
+  // State to manage the visibility of dropdown menu
   const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  // State to manage the value of the input
+  const [value, setvalue] = useState('');
+
+  // Method to set the value of the input
+  const handleChangeValue = value => {
+    setvalue(value);
+  };
+
+  // Method to toggle the dropdown
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <section>
       <div className='container'>
-        <input id='search' className='search' type='text' placeholder='Search for a country' />
-        <div id='filter' className='dropdown' onClick={() => setDropdownVisible(!dropdownVisible)}>
+        <input
+          className='search'
+          type='text'
+          placeholder='Search for a country'
+          value={value}
+          onChange={event => {
+            handleChangeValue(event.target.value);
+          }}
+        />
+        <div className='dropdown' onClick={toggleDropdown}>
           Filter by region
           <FontAwesomeIcon className='fas fa-chevron-down' icon={faChevronDown} />
           {dropdownVisible && (
