ed44026105e33ca041079d30a3164243397dcfd1
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-04 14:43:12 +0100

    feat(): Add method to filter countries by input value. Call it every time the input value change

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,13 +3,34 @@ import '../Style/Search.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Search = ({ value, handleChangeValue }) => {
+const Search = () => {
+  // State to manage the value of the input
+  const [value, setvalue] = useState('');
+
   // State to manage the visibility of dropdown menu
   const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  // Method to set the value of the input
+  const handleChangeValue = value => {
+    setvalue(value);
+    searchCountries(value);
+  };
 
   // Method to toggle the dropdown
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
+  };
+
+  // Method to hide or show countries by his name
+  const searchCountries = value => {
+    const countryName = document.querySelectorAll('.country-name');
+    countryName.forEach(name => {
+      if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+        name.parentElement.parentElement.style.display = 'block';
+      } else {
+        name.parentElement.parentElement.style.display = 'none';
+      }
+    });
   };
 
   return (
