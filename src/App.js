83796066f11db35746fa28f1f3dd17f182dad126
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-04 14:42:36 +0100

    refactor(): Re-export input state and method to set input state in the Search component because we don't need the Countries list state, we just hide or show dom elements.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Style/App.css';
 import Header from './Components/Header';
 import Search from './Components/Search';
 import List from './Components/List';
 
-const App = () => {
-  // State to manage the value of the input
-  const [value, setvalue] = useState('');
-
-  // Method to set the value of the input
-  const handleChangeValue = value => {
-    setvalue(value);
-  };
-
-  return (
-    <>
-      <Header />
-      <Search value={value} handleChangeValue={handleChangeValue} />
-      <List />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Header />
+    <Search />
+    <List />
+  </>
+);
 
 export default App;
